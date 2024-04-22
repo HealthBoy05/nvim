67d728365228be8222d7328acf60fbc8f6ec4d2e
gg <wurtzii05@gmail.com>
2024-04-22 18:51:44 -0500

    added pyright to list of installed language servers

--- lua/plugins/lsp_zero.lua
+++ lua/plugins/lsp_zero.lua
@@ -10,7 +10,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {'jdtls', 'rust_analyzer', 'clangd', 'lua_ls'},
+  ensure_installed = {'jdtls', 'rust_analyzer', 'clangd', 'lua_ls', 'pyright'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
